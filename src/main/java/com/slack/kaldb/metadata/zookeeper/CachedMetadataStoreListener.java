4d0b98d502961b9e2d58f7d96d54dfe11dbe6ae8
Suman Karumuri <skarumuri@slack-corp.com>
2021-07-27 10:57:27 -0700

    tiny

--- src/main/java/com/slack/kaldb/metadata/zookeeper/CachedMetadataStoreListener.java
+++ src/main/java/com/slack/kaldb/metadata/zookeeper/CachedMetadataStoreListener.java
@@ -2,7 +2,9 @@ package com.slack.kaldb.metadata.zookeeper;
 
 import org.apache.curator.framework.state.ConnectionStateListener;
 
-/** Listener for listening to changes in CachedMetadataStore */
+/**
+ * Listener for listening to changes in CachedMetadataStore TODO: prefix this class name with ZK.
+ */
 public interface CachedMetadataStoreListener extends ConnectionStateListener {
   /** Called when cache is changed (instances added/deleted or updated). */
   public void cacheChanged();
