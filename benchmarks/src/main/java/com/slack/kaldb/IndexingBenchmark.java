a6582a5f1e73ef69058b6a51b602328873498ab0
Varun Thacker <vthacker@users.noreply.github.com>
2021-08-05 22:32:40 -0700

    index to lucene directly and in order to achieve that use use multi module (#67)
    * index to lucene directly and in order to achieve that use use multi module

--- benchmarks/src/main/java/com/slack/kaldb/IndexingBenchmark.java
+++ benchmarks/src/main/java/com/slack/kaldb/IndexingBenchmark.java
@@ -16,6 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.stream.Stream;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexWriter;
 import org.openjdk.jmh.annotations.*;
 
 @State(Scope.Thread)
@@ -23,7 +25,6 @@ public class IndexingBenchmark {
 
   private final Duration commitInterval = Duration.ofSeconds(5 * 60);
   private final Duration refreshInterval = Duration.ofSeconds(5 * 60);
-  String timestamp;
 
   private Path tempDirectory;
   private MeterRegistry registry;
@@ -31,23 +32,41 @@ public class IndexingBenchmark {
   private Random random;
 
   private LogMessage logMessage;
+  private Document logDocument;
 
   @Setup(Level.Iteration)
   public void createIndexer() throws IOException {
     random = new Random();
-    timestamp = Instant.now().toString();
+    Instant timestamp = Instant.now();
     registry = new SimpleMeterRegistry();
     tempDirectory =
         Files.createDirectories(
-            Paths.get("jmh", String.valueOf(random.nextInt(Integer.MAX_VALUE))));
+            Paths.get(
+                "benchmarks", "jmh-output", String.valueOf(random.nextInt(Integer.MAX_VALUE))));
     logStore =
         LuceneIndexStoreImpl.makeLogStore(
             tempDirectory.toFile(), commitInterval, refreshInterval, registry);
 
     Map<String, Object> fieldMap = new HashMap<>();
-    fieldMap.put(LogMessage.ReservedField.TIMESTAMP.fieldName, timestamp);
+    fieldMap.put(LogMessage.ReservedField.TIMESTAMP.fieldName, timestamp.toString());
     fieldMap.put(LogMessage.ReservedField.MESSAGE.fieldName, "Log Message");
     logMessage = new LogMessage("testindex", "INFO", "1", fieldMap);
+
+    // inlines LogDocumentBuilderImpl#fromMessage
+    logDocument = new Document();
+    logDocument.add(new TextField("index", "testindex", Field.Store.NO));
+    logDocument.add(new TextField("id", "1", Field.Store.NO));
+    logDocument.add(new TextField("type", "INFO", Field.Store.NO));
+    logDocument.add(new TextField("message", "Log Message", Field.Store.NO));
+
+    logDocument.add(new LongPoint("_timesinceepoch", timestamp.toEpochMilli()));
+    logDocument.add(new NumericDocValuesField("_timesinceepoch", timestamp.toEpochMilli()));
+
+    String value =
+        "{\"id\":\"1\",\"source\":{\"@timestamp\":\""
+            + timestamp.toString()
+            + "\",\"message\":\"Log Message\"},\"index\":\"testindex\",\"type\":\"INFO\"}";
+    logDocument.add(new StoredField("_source", value));
   }
 
   @TearDown(Level.Iteration)
@@ -62,5 +81,15 @@ public class IndexingBenchmark {
   @Benchmark
   public void measureIndexing() {
     logStore.addMessage(logMessage);
+  }
+
+  @Benchmark
+  public void measureIndexingToLuceneDirectly() {
+    IndexWriter indexWriter = logStore.getIndexWriter();
+    try {
+      indexWriter.addDocument(logDocument);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
