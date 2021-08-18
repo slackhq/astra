be896645a67b7fc1a83208a7c518b0250e44ce56
Varun Thacker <varun@vthacker.in>
2021-08-17 17:13:36 -0700

    modifications to jmh.sh

--- benchmarks/jmh.sh
+++ benchmarks/jmh.sh
@@ -1,11 +1,8 @@
-gcArgs="-XX:+UseG1GC -Xms4g -Xmx4g -XX:MaxGCPauseMillis=10"
-loggingArgs="-Dlog4j.configurationFile=./log4j2.xml"
-
 echo "Building Project"
 mvn clean package -DskipTests=true --file ../pom.xml
 
 echo "JMH benchmarks start"
 
-exec java $loggingArgs $gcArgs -jar ../benchmarks/target/benchmarks.jar "$@"
+exec java -Dlog4j.configurationFile=./log4j2.xml -Xms4g -Xmx4g $gcArgs -jar ../benchmarks/target/benchmarks.jar "$@"
 
 echo "JMH benchmarks done"
