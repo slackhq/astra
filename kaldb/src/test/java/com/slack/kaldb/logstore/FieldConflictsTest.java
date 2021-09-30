67c5b3923ce7f5539ded507b63ed280a09c7e438
Suman Karumuri <skarumuri@slack-corp.com>
2021-09-29 18:48:00 -0700

    Add a unit test for field conflicts.

---
+++ kaldb/src/test/java/com/slack/kaldb/logstore/FieldConflictsTest.java
@@ -0,0 +1,179 @@
+package com.slack.kaldb.logstore;
+
+import static com.slack.kaldb.testlib.TemporaryLogStoreAndSearcherRule.findAllMessages;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import brave.Tracing;
+import com.slack.kaldb.testlib.MessageUtil;
+import com.slack.kaldb.testlib.TemporaryLogStoreAndSearcherRule;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class FieldConflictsTest {
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    Tracing.newBuilder().build();
+  }
+
+  @Rule
+  public TemporaryLogStoreAndSearcherRule strictLogStore =
+      new TemporaryLogStoreAndSearcherRule(false);
+
+  public FieldConflictsTest() throws IOException {}
+
+  @Test
+  public void testFieldConflictingFieldTypeWithSameValue() throws InterruptedException {
+    final String conflictingFieldName = "conflictingField";
+
+    LogMessage msg1 =
+        new LogMessage(
+            MessageUtil.TEST_INDEX_NAME,
+            "INFO",
+            "1",
+            Map.of(
+                LogMessage.ReservedField.TIMESTAMP.fieldName,
+                MessageUtil.getCurrentLogDate(),
+                LogMessage.ReservedField.MESSAGE.fieldName,
+                "Test message",
+                LogMessage.ReservedField.TAG.fieldName,
+                "foo-bar",
+                LogMessage.ReservedField.HOSTNAME.fieldName,
+                "host1-dc2.abc.com",
+                conflictingFieldName,
+                "1"));
+    strictLogStore.logStore.addMessage(msg1);
+
+    LogMessage msg2 =
+        new LogMessage(
+            MessageUtil.TEST_INDEX_NAME,
+            "INFO",
+            "1",
+            Map.of(
+                LogMessage.ReservedField.TIMESTAMP.fieldName,
+                MessageUtil.getCurrentLogDate(),
+                LogMessage.ReservedField.MESSAGE.fieldName,
+                "Test message",
+                LogMessage.ReservedField.TAG.fieldName,
+                "foo-bar",
+                LogMessage.ReservedField.HOSTNAME.fieldName,
+                "host1-dc2.abc.com",
+                conflictingFieldName,
+                1));
+    strictLogStore.logStore.addMessage(msg2);
+
+    strictLogStore.logStore.commit();
+    strictLogStore.logStore.refresh();
+    Thread.sleep(1000);
+
+    final String queryByHost = "hostname:host1-dc2.abc.com";
+    Collection<LogMessage> resultsByHost =
+        findAllMessages(
+            strictLogStore.logSearcher, MessageUtil.TEST_INDEX_NAME, queryByHost, 1000, 1);
+    assertThat(resultsByHost.size()).isEqualTo(2);
+
+    final String conflictingTypeByNumber = conflictingFieldName + ":1";
+    Collection<LogMessage> searchByInt =
+        findAllMessages(
+            strictLogStore.logSearcher,
+            MessageUtil.TEST_INDEX_NAME,
+            conflictingTypeByNumber,
+            1000,
+            1);
+    assertThat(searchByInt.size()).isEqualTo(2);
+
+    final String conflictingTypeExactMatch = conflictingFieldName + ":\"1\"";
+    Collection<LogMessage> searchByNumber =
+        findAllMessages(
+            strictLogStore.logSearcher,
+            MessageUtil.TEST_INDEX_NAME,
+            conflictingTypeExactMatch,
+            1000,
+            1);
+    assertThat(searchByNumber.size()).isEqualTo(2);
+  }
+
+  @Test
+  public void testFieldConflictingFieldTypeWithDifferentValue() throws InterruptedException {
+    final String conflictingFieldName = "conflictingField";
+
+    LogMessage msg1 =
+        new LogMessage(
+            MessageUtil.TEST_INDEX_NAME,
+            "INFO",
+            "1",
+            Map.of(
+                LogMessage.ReservedField.TIMESTAMP.fieldName,
+                MessageUtil.getCurrentLogDate(),
+                LogMessage.ReservedField.MESSAGE.fieldName,
+                "Test message",
+                LogMessage.ReservedField.TAG.fieldName,
+                "foo-bar",
+                LogMessage.ReservedField.HOSTNAME.fieldName,
+                "host1-dc2.abc.com",
+                conflictingFieldName,
+                "1"));
+    strictLogStore.logStore.addMessage(msg1);
+
+    LogMessage msg2 =
+        new LogMessage(
+            MessageUtil.TEST_INDEX_NAME,
+            "INFO",
+            "1",
+            Map.of(
+                LogMessage.ReservedField.TIMESTAMP.fieldName,
+                MessageUtil.getCurrentLogDate(),
+                LogMessage.ReservedField.MESSAGE.fieldName,
+                "Test message",
+                LogMessage.ReservedField.TAG.fieldName,
+                "foo-bar",
+                LogMessage.ReservedField.HOSTNAME.fieldName,
+                "host1-dc2.abc.com",
+                conflictingFieldName,
+                200));
+    strictLogStore.logStore.addMessage(msg2);
+
+    strictLogStore.logStore.commit();
+    strictLogStore.logStore.refresh();
+    Thread.sleep(1000);
+
+    final String queryByHost = "hostname:host1-dc2.abc.com";
+    Collection<LogMessage> resultsByHost =
+        findAllMessages(
+            strictLogStore.logSearcher, MessageUtil.TEST_INDEX_NAME, queryByHost, 1000, 1);
+    assertThat(resultsByHost.size()).isEqualTo(2);
+
+    final String conflictingTypeByString = conflictingFieldName + ":1";
+    Collection<LogMessage> searchByString =
+        findAllMessages(
+            strictLogStore.logSearcher,
+            MessageUtil.TEST_INDEX_NAME,
+            conflictingTypeByString,
+            1000,
+            1);
+    assertThat(searchByString.size()).isEqualTo(1);
+
+    final String conflictingTypeByNumber = conflictingFieldName + ":200";
+    Collection<LogMessage> searchByNumber =
+        findAllMessages(
+            strictLogStore.logSearcher,
+            MessageUtil.TEST_INDEX_NAME,
+            conflictingTypeByNumber,
+            1000,
+            1);
+    assertThat(searchByNumber.size()).isEqualTo(1);
+
+    final String conflictingTypeByNumberString = conflictingFieldName + ":\"200\"";
+    Collection<LogMessage> searchByNumberString =
+        findAllMessages(
+            strictLogStore.logSearcher,
+            MessageUtil.TEST_INDEX_NAME,
+            conflictingTypeByNumberString,
+            1000,
+            1);
+    assertThat(searchByNumberString.size()).isEqualTo(1);
+  }
+}
