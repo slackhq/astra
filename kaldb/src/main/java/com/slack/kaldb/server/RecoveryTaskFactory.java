184e43f183f3314f43542d3bb911b6a9f8b0f421
Suman Karumuri <skarumuri@slack-corp.com>
2021-12-09 14:39:47 -0800

    rename.

--- kaldb/src/main/java/com/slack/kaldb/server/RecoveryTaskFactory.java
+++ kaldb/src/main/java/com/slack/kaldb/server/RecoveryTaskFactory.java
@@ -1,20 +1,22 @@
 package com.slack.kaldb.server;
 
+import com.slack.kaldb.metadata.recovery.RecoveryTaskMetadata;
 import com.slack.kaldb.metadata.recovery.RecoveryTaskMetadataStore;
 import com.slack.kaldb.metadata.snapshot.SnapshotMetadata;
 import com.slack.kaldb.metadata.snapshot.SnapshotMetadataStore;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
  * This class is responsible for the indexer startup operations like stale live snapshot cleanup.
  * determining the start indexing offset and create Recovery task etc..
  */
-public class IndexerStartup {
+public class RecoveryTaskFactory {
   private final SnapshotMetadataStore snapshotMetadataStore;
   private final RecoveryTaskMetadataStore recoveryTaskMetadataStore;
 
-  public IndexerStartup(
+  public RecoveryTaskFactory(
       SnapshotMetadataStore snapshotMetadataStore,
       RecoveryTaskMetadataStore recoveryTaskMetadataStore) {
     this.snapshotMetadataStore = snapshotMetadataStore;
@@ -49,5 +51,19 @@ public class IndexerStartup {
     staleSnapshots
         .stream()
         .forEach(snapshotMetadata -> snapshotMetadataStore.deleteSync(snapshotMetadata));
+  }
+
+  public int getStartOffsetForPartition(String partitionId) {
+    List<SnapshotMetadata> snapshots = snapshotMetadataStore.listSync();
+    Optional<Long> maxSnapshotOffset = snapshots.stream()
+            .filter(snapshot -> snapshot.partitionId.equals(partitionId))
+            .collect(Collectors.maxBy((SnapshotMetadata x, SnapshotMetadata y) -> x.maxOffset - y.maxOffset));
+
+    List<RecoveryTaskMetadata> recoveryTasks = recoveryTaskMetadataStore.listSync();
+    Optional<Long> maxRecoveryOffset = recoveryTasks.stream()
+            .filter(recoveryTaskMetadata -> recoveryTaskMetadata.partitionId.equals(recoveryTaskMetadata))
+            .collect(Collectors.maxBy((x, y) -> x.endOffset - y.endOffset));
+
+    return Math.max(maxRecoveryOffset.orElse(0), maxRecoveryOffset.orElse(0));
   }
 }
