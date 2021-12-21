7a83664b810de1dc225c424d8473fa7daa0bda83
Varun Thacker <varun@vthacker.in>
2021-12-21 12:19:34 -0800

    shutdown log4j correctly

--- kaldb/src/main/java/com/slack/kaldb/util/RuntimeHalterImpl.java
+++ kaldb/src/main/java/com/slack/kaldb/util/RuntimeHalterImpl.java
@@ -8,7 +8,6 @@ public class RuntimeHalterImpl implements FatalErrorHandler {
 
   @Override
   public void handleFatal(Throwable t) {
-    t.printStackTrace();
     LOG.error("Runtime halter is called probably on an unrecoverable error. Stopping the VM.", t);
     // TODO: Make the exit more cleaner, once the system is more stable.
     System.exit(1);
