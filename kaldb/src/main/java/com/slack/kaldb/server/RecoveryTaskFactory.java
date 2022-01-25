2414302e1628d12f0486f9a02ff94279cb421515
Suman Karumuri <skarumuri@slack-corp.com>
2022-01-25 05:35:36 -0800

    Add logic for setting start offset.

--- kaldb/src/main/java/com/slack/kaldb/server/RecoveryTaskFactory.java
+++ kaldb/src/main/java/com/slack/kaldb/server/RecoveryTaskFactory.java
@@ -1,5 +1,6 @@
 package com.slack.kaldb.server;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.slack.kaldb.metadata.recovery.RecoveryTaskMetadata;
 import com.slack.kaldb.metadata.recovery.RecoveryTaskMetadataStore;
 import com.slack.kaldb.metadata.snapshot.SnapshotMetadata;
@@ -34,6 +35,7 @@ public class RecoveryTaskFactory {
     this.maxOffsetDelay = maxOffsetDelay;
   }
 
+  @VisibleForTesting
   public List<SnapshotMetadata> getStaleLiveSnapshots(List<SnapshotMetadata> snapshots) {
     List<SnapshotMetadata> snapshotsForPartition =
         snapshots
@@ -46,21 +48,23 @@ public class RecoveryTaskFactory {
         .collect(Collectors.toUnmodifiableList());
   }
 
-  public int deleteStaleLiveSnapsnots(List<SnapshotMetadata> snapshots) {
+  @VisibleForTesting
+  public List<SnapshotMetadata> deleteStaleLiveSnapsnots(List<SnapshotMetadata> snapshots) {
     List<SnapshotMetadata> staleSnapshots = getStaleLiveSnapshots(snapshots);
     LOG.info("Deleting {} stale snapshots: {}", staleSnapshots.size(), staleSnapshots);
-    return SnapshotsUtil.deleteSnapshots(snapshotMetadataStore, staleSnapshots);
+    int deletedSnapshotCount = SnapshotsUtil.deleteSnapshots(snapshotMetadataStore, staleSnapshots);
+
+    int failedDeletes = staleSnapshots.size() - deletedSnapshotCount;
+    if (failedDeletes > 0) {
+      LOG.warn("Failed to delete {} live snapshots", failedDeletes);
+      throw new IllegalStateException("Failed to delete stale live snapshots");
+    }
+
+    return staleSnapshots;
   }
 
-  /*
-  TODO: Implement start up operation.
-  TODO: Implement recovery task creation.
-  start up tasks:
-      * Cleanup stale live nodes.
-      * Create a recovery task if needed.
-      * Get max offset to start indexing.
-   */
   // Get the highest offset for which data is durable for a partition.
+  @VisibleForTesting
   public long getHigestDurableOffsetForPartition(
       List<SnapshotMetadata> snapshots, List<RecoveryTaskMetadata> recoveryTasks) {
 
@@ -84,27 +88,79 @@ public class RecoveryTaskFactory {
   }
 
   /**
-   * If the current index offset is more than the configured delay, we can't catch up indexing. So,
-   * instead of trying to catch up, create a recovery task and start indexing at the current head.
-   * This strategy achieves 2 goals: we start indexing fresh data when we are behind and we add more
-   * indexing capacity when needed.
+   * To determine the start offset, an indexer performs multiple tasks. First, we clean up all the
+   * stale live nodes for this partition so there is only 1 live node per indexer.
+   *
+   * <p>In Kaldb, the durability of unindexed data is ensured by Kafka and the durability of indexed
+   * data is ensured by S3. So, once the indexer restarts, we need to determine the highest offset
+   * that was indexed. To get the latest indexed offset, we get the latest indexed offset from a
+   * snapshots for that partition. Since there could also be a recovery task queued up for this
+   * partition, we also need to skip the offsets picked up by the recovery task. So, the highest
+   * durable offset is the highest offset for a partition among the snapshots and recovery tasks for
+   * a partition.
+   *
+   * <p>The highest durable offset is the start offset for the indexer. If this offset is with in
+   * the max start delay of the head, we start indexing. If the current index offset is more than
+   * the configured delay, we can't catch up indexing. So, instead of trying to catch up, create a
+   * recovery task and start indexing at the current head. This strategy achieves 2 goals: we start
+   * indexing fresh data when we are behind and we add more indexing capacity when needed. The
+   * recovery task offsets are [startOffset, endOffset). If a recovery task is created, we start
+   * indexing at the offset after the recovery task.
    */
-  public long getStartOffset(long currentOffset, long currentHeadOffset) {
-    if (currentHeadOffset - currentOffset > maxOffsetDelay) {
-      // TODO: Name task better.
-      // TODO: created time is in ms?
-      // TODO: are offsets inclusive? current head offset -1?
-      // TODO: Make sure the offsets are inclusive
+  public long determineStartingOffset(long currentHeadOffsetForPartition) {
+    // Filter stale snapshots for partition.
+    List<SnapshotMetadata> snapshots = snapshotMetadataStore.listSync();
+    List<SnapshotMetadata> snapshotsForPartition =
+        snapshots
+            .stream()
+            .filter(snapshotMetadata -> snapshotMetadata.partitionId.equals(partitionId))
+            .collect(Collectors.toUnmodifiableList());
+    List<SnapshotMetadata> deletedSnapshots = deleteStaleLiveSnapsnots(snapshotsForPartition);
+
+    List<SnapshotMetadata> nonLiveSnapshotsForPartition =
+        snapshotsForPartition
+            .stream()
+            .filter(deletedSnapshots::contains)
+            .collect(Collectors.toUnmodifiableList());
+
+    // Get the highest offset that is indexed in durable store.
+    List<RecoveryTaskMetadata> recoveryTasks = recoveryTaskMetadataStore.listSync();
+    long highestDuableOffsetForPartition =
+        getHigestDurableOffsetForPartition(nonLiveSnapshotsForPartition, recoveryTasks);
+    LOG.info(
+        "The highest durable offset for partition {} is {}",
+        partitionId,
+        highestDuableOffsetForPartition);
+
+    // Create a recovery task if needed.
+    if (currentHeadOffsetForPartition - highestDuableOffsetForPartition > maxOffsetDelay) {
+      final long creationTimeEpochMs = Instant.now().toEpochMilli();
+      final String recoveryTaskName = "recoveryTask_" + partitionId + "_" + creationTimeEpochMs;
+      LOG.info(
+          "The current position {} and head location {} are higher than max offset {}",
+          highestDuableOffsetForPartition,
+          currentHeadOffsetForPartition,
+          maxOffsetDelay);
       recoveryTaskMetadataStore.createSync(
           new RecoveryTaskMetadata(
-              "recoveryTask" + partitionId,
+              recoveryTaskName,
               partitionId,
-              currentOffset,
-              currentHeadOffset,
-              Instant.now().toEpochMilli()));
-      return currentHeadOffset;
+              highestDuableOffsetForPartition,
+              currentHeadOffsetForPartition,
+              creationTimeEpochMs));
+      LOG.info(
+          "Created recovery task {} to catchup. Moving the starting offset to head at {}",
+          recoveryTaskName,
+          currentHeadOffsetForPartition);
+      return currentHeadOffsetForPartition;
     } else {
-      return currentOffset;
+      LOG.info(
+          "The current position {} and head location {} are lower than max offset {}. Using "
+              + "current position as start offset",
+          highestDuableOffsetForPartition,
+          currentHeadOffsetForPartition,
+          maxOffsetDelay);
+      return highestDuableOffsetForPartition;
     }
   }
 }
