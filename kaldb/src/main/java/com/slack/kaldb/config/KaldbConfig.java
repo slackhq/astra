c0cd57866a1a6681ccc2b7f1a09ba2fd541c6e12
Suman Karumuri <skarumuri@slack-corp.com>
2022-02-08 17:15:34 -0800

    Refactor config.

--- kaldb/src/main/java/com/slack/kaldb/config/KaldbConfig.java
+++ kaldb/src/main/java/com/slack/kaldb/config/KaldbConfig.java
@@ -1,13 +1,17 @@
 package com.slack.kaldb.config;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import com.slack.kaldb.proto.config.KaldbConfigs;
+import com.slack.kaldb.writer.LogMessageTransformer;
+import com.slack.kaldb.writer.LogMessageWriterImpl;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,15 +42,6 @@ public class KaldbConfig {
     return kaldbConfig;
   }
 
-  public static void validateConfig(KaldbConfigs.KaldbConfig kaldbConfig) {
-    // We don't need further checks for node roles since JSON parsing will throw away roles not part
-    // of the enum
-    Preconditions.checkArgument(
-        !kaldbConfig.getNodeRolesList().isEmpty(),
-        "Kaldb must start with atleast 1 node role. Accepted roles are "
-            + Arrays.toString(KaldbConfigs.NodeRole.values()));
-  }
-
   // Parse a yaml string as a KaldbConfig proto struct
   @VisibleForTesting
   static KaldbConfigs.KaldbConfig fromYamlConfig(String yamlStr)
@@ -57,6 +52,45 @@ public class KaldbConfig {
 
     Object obj = yamlReader.readValue(substitute.replace(yamlStr), Object.class);
     return fromJsonConfig(jsonWriter.writeValueAsString(obj));
+  }
+
+  /**
+   * ValidateConfig ensures that various config values across classes are consistent. The class
+   * using a config is still expected to ensure the config values are valid. For example, the roles
+   * can't be empty.
+   */
+  public static void validateConfig(KaldbConfigs.KaldbConfig kaldbConfig) {
+    validateNodeRoles(kaldbConfig.getNodeRolesList());
+    validateDataTransformerConfig(kaldbConfig.getIndexerConfig().getDataTransformer());
+  }
+
+  public static void validateNodeRoles(List<KaldbConfigs.NodeRole> nodeRoleList) {
+    // We don't need further checks for node roles since JSON parsing will throw away roles not part
+    // of the enum
+    checkArgument(
+        !nodeRoleList.isEmpty(),
+        "Kaldb must start with atleast 1 node role. Accepted roles are "
+            + Arrays.toString(KaldbConfigs.NodeRole.values()));
+  }
+
+  // TODO: Private var?
+  @VisibleForTesting
+  public static final Map<String, LogMessageTransformer> dataTransformerMap =
+      ImmutableMap.of(
+          "api_log",
+          LogMessageWriterImpl.apiLogTransformer,
+          "spans",
+          LogMessageWriterImpl.spanTransformer,
+          "json",
+          LogMessageWriterImpl.jsonLogMessageTransformer);
+
+  public static void validateDataTransformerConfig(String dataTransformerConfig) {
+    checkArgument(
+        dataTransformerConfig.isEmpty(),
+        "IndexerConfig can't have an empty dataTransformer config.");
+    checkArgument(
+        !dataTransformerConfig.contains(dataTransformerConfig),
+        "Invalid data transformer config: " + dataTransformerConfig);
   }
 
   @VisibleForTesting
