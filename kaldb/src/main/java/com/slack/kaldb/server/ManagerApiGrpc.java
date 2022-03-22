6b843b4e98dbcd6669a1aed74c83ad2f5b0d6a62
Bryan Burkholder <bryanlb@users.noreply.github.com>
2022-03-22 15:27:31 -0700

    Add remaining manager tests, fix partition bug

--- kaldb/src/main/java/com/slack/kaldb/server/ManagerApiGrpc.java
+++ kaldb/src/main/java/com/slack/kaldb/server/ManagerApiGrpc.java
@@ -1,5 +1,6 @@
 package com.slack.kaldb.server;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.slack.kaldb.metadata.service.ServiceMetadata;
 import com.slack.kaldb.metadata.service.ServiceMetadataStore;
@@ -11,6 +12,7 @@ import io.grpc.stub.StreamObserver;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -103,16 +105,51 @@ public class ManagerApiGrpc extends ManagerApiServiceGrpc.ManagerApiServiceImplB
       ManagerApi.AddServicePartitionRequest request,
       StreamObserver<ManagerApi.AddServicePartitionResponse> responseObserver) {
     try {
+      Preconditions.checkArgument(
+          request.getPartitionIdsList().size() > 0, "PartitionIds list must not be empty");
+      Preconditions.checkArgument(
+          request.getPartitionIdsList().stream().noneMatch(String::isBlank),
+          "PartitionIds list must not contain blank strings");
+
       ServiceMetadata serviceMetadata = serviceMetadataStore.getNodeSync(request.getName());
-      ServicePartitionMetadata servicePartitionMetadata =
+      Optional<ServicePartitionMetadata> previousActiveServicePartition =
+          serviceMetadata
+              .getPartitionConfigs()
+              .stream()
+              .filter(
+                  servicePartitionMetadata ->
+                      servicePartitionMetadata.getEndTimeEpochMs() == Long.MAX_VALUE)
+              .findFirst();
+      List<ServicePartitionMetadata> remainingServicePartitions =
+          serviceMetadata
+              .getPartitionConfigs()
+              .stream()
+              .filter(
+                  servicePartitionMetadata ->
+                      servicePartitionMetadata.getEndTimeEpochMs() != Long.MAX_VALUE)
+              .collect(Collectors.toList());
+
+      // todo - consider adding some padding to this value; this may complicate
+      //   validation as you would need to consider what happens when there's a future
+      //   cut-over already scheduled
+      long partitionCutoverTime = Instant.now().toEpochMilli();
+
+      ImmutableList.Builder<ServicePartitionMetadata> builder =
+          ImmutableList.<ServicePartitionMetadata>builder().addAll(remainingServicePartitions);
+      if (previousActiveServicePartition.isPresent()) {
+        ServicePartitionMetadata updatedPreviousActivePartition =
+            new ServicePartitionMetadata(
+                previousActiveServicePartition.get().getStartTimeEpochMs(),
+                partitionCutoverTime,
+                previousActiveServicePartition.get().getPartitions());
+        builder.add(updatedPreviousActivePartition);
+      }
+
+      ServicePartitionMetadata newPartitionMetadata =
           new ServicePartitionMetadata(
-              // todo - should this have a fixed amount of padding applied?
-              Instant.now().toEpochMilli(), Long.MAX_VALUE, request.getPartitionIdsList());
+              partitionCutoverTime + 1, Long.MAX_VALUE, request.getPartitionIdsList());
       ImmutableList<ServicePartitionMetadata> updatedServicePartitionMetadata =
-          ImmutableList.<ServicePartitionMetadata>builder()
-              .addAll(serviceMetadata.getPartitionConfigs())
-              .add(servicePartitionMetadata)
-              .build();
+          builder.add(newPartitionMetadata).build();
 
       ServiceMetadata updatedServiceMetadata =
           new ServiceMetadata(
@@ -122,7 +159,11 @@ public class ManagerApiGrpc extends ManagerApiServiceGrpc.ManagerApiServiceImplB
               updatedServicePartitionMetadata);
 
       serviceMetadataStore.updateSync(updatedServiceMetadata);
-      responseObserver.onNext(ManagerApi.AddServicePartitionResponse.newBuilder().build());
+      responseObserver.onNext(
+          ManagerApi.AddServicePartitionResponse.newBuilder()
+              .setStartTimeEpochMs(newPartitionMetadata.getStartTimeEpochMs())
+              .addAllPartitionIds(newPartitionMetadata.getPartitions())
+              .build());
       responseObserver.onCompleted();
     } catch (Exception e) {
       LOG.error("Error adding service partition", e);
--- kaldb/src/test/java/com/slack/kaldb/server/ManagerApiGrpcTest.java
+++ kaldb/src/test/java/com/slack/kaldb/server/ManagerApiGrpcTest.java
@@ -19,6 +19,9 @@ import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
 import org.apache.curator.test.TestingServer;
 import org.junit.After;
 import org.junit.Before;
@@ -275,24 +278,214 @@ public class ManagerApiGrpcTest {
     assertThat(status.getDescription()).isEqualTo("throughputBytes must be greater than 0");
   }
 
-  // todo
-  public void shouldErrorGettingNonexistentService() {}
+  @Test
+  public void shouldErrorGettingNonexistentService() {
+    StatusRuntimeException throwable =
+        (StatusRuntimeException)
+            catchThrowable(
+                () ->
+                    managerApiStub.getService(
+                        ManagerApi.GetServiceRequest.newBuilder().setName("foo").build()));
+    Status status = throwable.getStatus();
+    assertThat(status.getCode()).isEqualTo(Status.UNKNOWN.getCode());
+  }
 
-  // todo
-  public void shouldAppendServicePartitions() {}
+  @Test
+  public void shouldAppendServicePartitions() {
+    String serviceName = "testService";
+    String serviceOwner = "testOwner";
+    long serviceBytes = 1;
 
-  // todo
-  public void shouldErrorWithInvalidPartitionAssignment() {}
+    managerApiStub.createService(
+        ManagerApi.CreateServiceRequest.newBuilder()
+            .setName(serviceName)
+            .setOwner(serviceOwner)
+            .setThroughputBytes(serviceBytes)
+            .build());
 
-  // todo
-  public void shouldErrorAppendingPartitionsNonexistentService() {}
+    ManagerApi.GetServicePartitionsResponse partitionsResponse1 =
+        managerApiStub.getServicePartitions(
+            ManagerApi.GetServicePartitionsRequest.newBuilder().setName(serviceName).build());
+    assertThat(partitionsResponse1.getServicePartitionsList().size()).isEqualTo(0);
 
-  // todo
-  public void shouldListExistingPartitions() {}
+    long nowMs = Instant.now().toEpochMilli();
+    List<String> partitionList = List.of("1", "2");
+    ManagerApi.AddServicePartitionResponse addServicePartitionResponse =
+        managerApiStub.addServicePartition(
+            ManagerApi.AddServicePartitionRequest.newBuilder()
+                .setName(serviceName)
+                .addAllPartitionIds(partitionList)
+                .build());
 
-  // todo
-  public void shouldErrorListingPartitionsNonexistentService() {}
+    assertThat(addServicePartitionResponse.getStartTimeEpochMs()).isGreaterThanOrEqualTo(nowMs);
+    assertThat(addServicePartitionResponse.getPartitionIdsList()).isEqualTo(partitionList);
 
-  // todo
-  public void shouldListExistingServices() {}
+    ManagerApi.GetServicePartitionsResponse partitionsResponse2 =
+        managerApiStub.getServicePartitions(
+            ManagerApi.GetServicePartitionsRequest.newBuilder().setName(serviceName).build());
+    assertThat(partitionsResponse2.getServicePartitionsList().size()).isEqualTo(1);
+    assertThat(partitionsResponse2.getServicePartitionsList().get(0).getPartitionIdsList())
+        .isEqualTo(partitionList);
+  }
+
+  @Test
+  public void shouldErrorWithInvalidPartitionAssignment() {
+    String serviceName = "testService";
+    String serviceOwner = "testOwner";
+    long serviceBytes = 1;
+
+    managerApiStub.createService(
+        ManagerApi.CreateServiceRequest.newBuilder()
+            .setName(serviceName)
+            .setOwner(serviceOwner)
+            .setThroughputBytes(serviceBytes)
+            .build());
+
+    ManagerApi.GetServicePartitionsResponse partitionsResponse1 =
+        managerApiStub.getServicePartitions(
+            ManagerApi.GetServicePartitionsRequest.newBuilder().setName(serviceName).build());
+    assertThat(partitionsResponse1.getServicePartitionsList().size()).isEqualTo(0);
+
+    StatusRuntimeException throwable1 =
+        (StatusRuntimeException)
+            catchThrowable(
+                () ->
+                    managerApiStub.addServicePartition(
+                        ManagerApi.AddServicePartitionRequest.newBuilder()
+                            .setName(serviceName)
+                            .addAllPartitionIds(Collections.emptyList())
+                            .build()));
+    assertThat(throwable1.getStatus().getCode()).isEqualTo(Status.UNKNOWN.getCode());
+    assertThat(throwable1.getStatus().getDescription())
+        .isEqualTo("PartitionIds list must not be empty");
+
+    StatusRuntimeException throwable2 =
+        (StatusRuntimeException)
+            catchThrowable(
+                () ->
+                    managerApiStub.addServicePartition(
+                        ManagerApi.AddServicePartitionRequest.newBuilder()
+                            .setName(serviceName)
+                            .addAllPartitionIds(Collections.singletonList(""))
+                            .build()));
+    assertThat(throwable2.getStatus().getCode()).isEqualTo(Status.UNKNOWN.getCode());
+    assertThat(throwable2.getStatus().getDescription())
+        .isEqualTo("PartitionIds list must not contain blank strings");
+  }
+
+  @Test
+  public void shouldErrorAppendingPartitionsNonexistentService() {
+    String serviceName = "testService";
+    List<String> partitionList = List.of("1", "2");
+
+    StatusRuntimeException throwable1 =
+        (StatusRuntimeException)
+            catchThrowable(
+                () ->
+                    managerApiStub.addServicePartition(
+                        ManagerApi.AddServicePartitionRequest.newBuilder()
+                            .setName(serviceName)
+                            .addAllPartitionIds(partitionList)
+                            .build()));
+    assertThat(throwable1.getStatus().getCode()).isEqualTo(Status.UNKNOWN.getCode());
+  }
+
+  @Test
+  public void shouldListExistingPartitions() {
+    String serviceName = "testService";
+    String serviceOwner = "testOwner";
+    long serviceBytes = 1;
+
+    managerApiStub.createService(
+        ManagerApi.CreateServiceRequest.newBuilder()
+            .setName(serviceName)
+            .setOwner(serviceOwner)
+            .setThroughputBytes(serviceBytes)
+            .build());
+
+    List<String> partitionList1 = List.of("1", "2");
+    managerApiStub.addServicePartition(
+        ManagerApi.AddServicePartitionRequest.newBuilder()
+            .setName(serviceName)
+            .addAllPartitionIds(partitionList1)
+            .build());
+
+    List<String> partitionList2 = List.of("3", "4");
+    managerApiStub.addServicePartition(
+        ManagerApi.AddServicePartitionRequest.newBuilder()
+            .setName(serviceName)
+            .addAllPartitionIds(partitionList2)
+            .build());
+
+    ManagerApi.GetServicePartitionsResponse partitionsResponse =
+        managerApiStub.getServicePartitions(
+            ManagerApi.GetServicePartitionsRequest.newBuilder().setName(serviceName).build());
+    assertThat(partitionsResponse.getServicePartitionsList().size()).isEqualTo(2);
+
+    assertThat(partitionsResponse.getServicePartitionsList().get(0).getPartitionIdsList())
+        .isEqualTo(partitionList1);
+    assertThat(partitionsResponse.getServicePartitionsList().get(1).getPartitionIdsList())
+        .isEqualTo(partitionList2);
+    assertThat(partitionsResponse.getServicePartitionsList().get(0).getEndTimeEpochMs() + 1)
+        .isEqualTo(partitionsResponse.getServicePartitionsList().get(1).getStartTimeEpochMs());
+  }
+
+  @Test
+  public void shouldErrorListingPartitionsNonexistentService() {
+    String serviceName = "testService";
+
+    StatusRuntimeException throwable =
+        (StatusRuntimeException)
+            catchThrowable(
+                () ->
+                    managerApiStub.getServicePartitions(
+                        ManagerApi.GetServicePartitionsRequest.newBuilder()
+                            .setName(serviceName)
+                            .build()));
+    assertThat(throwable.getStatus().getCode()).isEqualTo(Status.UNKNOWN.getCode());
+  }
+
+  @Test
+  public void shouldListExistingServices() {
+    String serviceName1 = "testService1";
+    String serviceOwner1 = "testOwner1";
+    long serviceBytes1 = 1;
+
+    managerApiStub.createService(
+        ManagerApi.CreateServiceRequest.newBuilder()
+            .setName(serviceName1)
+            .setOwner(serviceOwner1)
+            .setThroughputBytes(serviceBytes1)
+            .build());
+
+    String serviceName2 = "testService2";
+    String serviceOwner2 = "testOwner2";
+    long serviceBytes2 = 2;
+
+    managerApiStub.createService(
+        ManagerApi.CreateServiceRequest.newBuilder()
+            .setName(serviceName2)
+            .setOwner(serviceOwner2)
+            .setThroughputBytes(serviceBytes2)
+            .build());
+
+    ManagerApi.GetServicesResponse getServicesResponse =
+        managerApiStub.getServices(ManagerApi.GetServicesRequest.newBuilder().build());
+
+    assertThat(
+        getServicesResponse
+            .getServiceListList()
+            .containsAll(
+                List.of(
+                    ManagerApi.GetServicesResponse.ServiceResponse.newBuilder()
+                        .setName(serviceName1)
+                        .setOwner(serviceOwner1)
+                        .setThroughputBytes(serviceBytes1)
+                        .build(),
+                    ManagerApi.GetServicesResponse.ServiceResponse.newBuilder()
+                        .setName(serviceName2)
+                        .setOwner(serviceOwner2)
+                        .setThroughputBytes(serviceBytes2)
+                        .build())));
+  }
 }
