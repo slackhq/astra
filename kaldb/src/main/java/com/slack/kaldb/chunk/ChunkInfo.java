6cd5bc845e423dda3d1a96a30fbdfc15aff2e124
Varun Thacker <varun@vthacker.in>
2022-05-25 11:07:40 -0700

    don't format string always

--- kaldb/src/main/java/com/slack/kaldb/chunk/ChunkInfo.java
+++ kaldb/src/main/java/com/slack/kaldb/chunk/ChunkInfo.java
@@ -176,10 +176,12 @@ public class ChunkInfo {
       long dataStartTimeEpochMs, long dataEndTimeEpochMs, long startTimeMs, long endTimeMs) {
     ensureTrue(endTimeMs >= 0, "end timestamp should be greater than zero: " + endTimeMs);
     ensureTrue(startTimeMs >= 0, "start timestamp should be greater than zero: " + startTimeMs);
-    ensureTrue(
-        endTimeMs - startTimeMs >= 0,
-        String.format(
-            "end timestamp %d can't be less than the start timestamp %d.", endTimeMs, startTimeMs));
+    if (endTimeMs - startTimeMs < 0) {
+      throw new IllegalArgumentException(
+          String.format(
+              "end timestamp %d can't be less than the start timestamp %d.",
+              endTimeMs, startTimeMs));
+    }
     return (dataStartTimeEpochMs <= startTimeMs && dataEndTimeEpochMs >= startTimeMs)
         || (dataStartTimeEpochMs <= endTimeMs && dataEndTimeEpochMs >= endTimeMs)
         || (dataStartTimeEpochMs >= startTimeMs && dataEndTimeEpochMs <= endTimeMs);
