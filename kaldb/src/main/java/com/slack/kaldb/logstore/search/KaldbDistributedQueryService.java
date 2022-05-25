e1f5ae648e84f90fa0200abbdb8a6815fe4a92b6
Varun Thacker <varun@vthacker.in>
2022-05-25 10:36:18 -0700

    search 1 node only once (#298)

--- kaldb/src/main/java/com/slack/kaldb/logstore/search/KaldbDistributedQueryService.java
+++ kaldb/src/main/java/com/slack/kaldb/logstore/search/KaldbDistributedQueryService.java
@@ -187,7 +187,7 @@ public class KaldbDistributedQueryService extends KaldbQueryServiceBase {
             .values()
             .stream()
             .map(KaldbDistributedQueryService::pickSearchNodeToQuery)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
     pickSearchNodeToQuerySpan.finish();
 
     return nodes;
