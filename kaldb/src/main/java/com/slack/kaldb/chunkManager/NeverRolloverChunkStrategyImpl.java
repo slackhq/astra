fbfb1dcac7560261a873d26340f0a084458475da
Suman Karumuri <skarumuri@salesforce.com>
2022-06-01 06:56:37 -0700

    tiny

--- kaldb/src/main/java/com/slack/kaldb/chunkManager/NeverRolloverChunkStrategyImpl.java
+++ kaldb/src/main/java/com/slack/kaldb/chunkManager/NeverRolloverChunkStrategyImpl.java
@@ -1,5 +1,9 @@
 package com.slack.kaldb.chunkManager;
 
+/**
+ * The NeverRolloverChunkStrategyImpl always responds in the negative for a chunk roll over request.
+ * It is currently used in recovery service which doesn't roll over the chunk.
+ */
 public class NeverRolloverChunkStrategyImpl implements ChunkRollOverStrategy {
   @Override
   public boolean shouldRollOver(long currentBytesIndexed, long currentMessagesIndexed) {
