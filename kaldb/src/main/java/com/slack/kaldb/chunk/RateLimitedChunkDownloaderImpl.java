0be012bbb031bfd2b8e9c1d29dada6e4cd41826c
Suman Karumuri <skarumuri@salesforce.com>
2022-06-07 09:58:41 -0700

    Add logging.

--- kaldb/src/main/java/com/slack/kaldb/chunk/RateLimitedChunkDownloaderImpl.java
+++ kaldb/src/main/java/com/slack/kaldb/chunk/RateLimitedChunkDownloaderImpl.java
@@ -2,12 +2,16 @@ package com.slack.kaldb.chunk;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * RateLimitedChunkDownloaderImpl is a decorator around a chunk downloader, that limits the number
  * chunk downloaders that can execute in parallel.
  */
 public class RateLimitedChunkDownloaderImpl implements ChunkDownloader {
+  private static final Logger LOG = LoggerFactory.getLogger(RateLimitedChunkDownloaderImpl.class);
+
   private final ExecutorService executor;
   private final ChunkDownloader chunkDownloader;
 
@@ -19,7 +23,10 @@ public class RateLimitedChunkDownloaderImpl implements ChunkDownloader {
 
   @Override
   public boolean download() throws Exception {
+    LOG.info("Starting rate limited chunk download.");
     Future<Boolean> future = executor.submit(chunkDownloader::download);
-    return future.get();
+    Boolean result = future.get();
+    LOG.info("Finished rate limited chunk download.");
+    return result;
   }
 }
