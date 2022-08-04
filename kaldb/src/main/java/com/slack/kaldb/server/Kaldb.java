987528a36e4793030aa9f081a539492301c02fac
Shelly Wu <54091662+shellywu815@users.noreply.github.com>
2022-08-04 13:00:12 -0700

    Added the skeleton code for functions in ZipkinService.java and added… (#347)
    * Added the skeleton code for functions in ZipkinService.java and added a zipkinService instance into Kaldb.java
    
    * solved formatting issues that caused build failures
    
    * fixed ZipkinServiceTest issue
    
    * deleted test messages

--- kaldb/src/main/java/com/slack/kaldb/server/Kaldb.java
+++ kaldb/src/main/java/com/slack/kaldb/server/Kaldb.java
@@ -27,6 +27,7 @@ import com.slack.kaldb.preprocessor.PreprocessorService;
 import com.slack.kaldb.proto.config.KaldbConfigs;
 import com.slack.kaldb.recovery.RecoveryService;
 import com.slack.kaldb.util.RuntimeHalterImpl;
+import com.slack.kaldb.zipkinApi.ZipkinService;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
@@ -208,6 +209,7 @@ public class Kaldb {
               .withRequestTimeout(requestTimeout)
               .withTracing(kaldbConfig.getTracingConfig())
               .withAnnotatedService(new ElasticsearchApiService(kaldbDistributedQueryService))
+              .withAnnotatedService(new ZipkinService(kaldbDistributedQueryService))
               .withGrpcService(kaldbDistributedQueryService)
               .build();
       services.add(armeriaService);
