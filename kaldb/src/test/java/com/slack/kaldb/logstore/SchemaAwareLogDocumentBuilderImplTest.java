7a730adc4d3452fdf0d3b00323a9d517289f8513
Suman Karumuri <skarumuri@salesforce.com>
2022-10-03 11:19:26 -0700

    tiny

---
+++ kaldb/src/test/java/com/slack/kaldb/logstore/SchemaAwareLogDocumentBuilderImplTest.java
@@ -0,0 +1,19 @@
+package com.slack.kaldb.logstore;
+
+import java.io.IOException;
+import org.junit.Before;
+
+public class SchemaAwareLogDocumentBuilderImplTest {
+
+  @Before
+  public void setup() throws IOException {
+    // testBuilderAllowExceptions = SchemaAwareLogDocumentBuilderImpl.build(false);
+    // testMessage = MessageUtil.makeMessage(0);
+  }
+
+  public void testDocumentCreation() {
+    // Document testDocument = testBuilderAllowExceptions.fromMessage(MessageUtil.makeMessage
+    // (0));
+    // assertThat(testDocument.getFields().size()).isEqualTo(12);
+  }
+}
