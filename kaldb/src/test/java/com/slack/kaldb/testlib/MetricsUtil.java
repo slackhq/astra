bf7596e694f1b2dc2e477231dca4b066d3055f6d
Varun Thacker <varun@vthacker.in>
2022-10-03 11:20:33 -0700

    code changes

--- kaldb/src/test/java/com/slack/kaldb/testlib/MetricsUtil.java
+++ kaldb/src/test/java/com/slack/kaldb/testlib/MetricsUtil.java
@@ -6,6 +6,25 @@ import io.micrometer.core.instrument.search.MeterNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// The get*() functions do some exception handling.
+// most likely we'll be calling getCount from an await() and waiting for the counter to match
+// expected value. When the meter has not been initialized we want await() to actually wait
+// till the code initializes the meter.
+// If we don't add a catch block we'll throw MeterNotFoundException and await() code needs
+// special handling to ensure the exception is dealt with
+// so instead of every called doing something like this below we just return 0
+//      await()
+//              .until(
+//                      () -> {
+//                        try {
+//                          double count = getCount(METRIC_NAME, meterRegistry);
+//                          LOG.debug("METRIC_NAME current_count={} total_count={}", count,
+// meterRegistry);
+//                          return count == <count_expected>;
+//                        } catch (MeterNotFoundException e) {
+//                          return false;
+//                        }
+//                      });
 public class MetricsUtil {
 
   private static final Logger LOG = LoggerFactory.getLogger(MetricsUtil.class);
@@ -13,44 +32,36 @@ public class MetricsUtil {
   public static double getCount(String counterName, MeterRegistry metricsRegistry) {
     try {
       return metricsRegistry.get(counterName).counter().count();
-    } catch (Exception e) {
-      LOG.info("Metric not found");
-      // most likely we'll be calling getCount from a await() and waiting for the counter to match
-      // expected value
-      // Now the thing is, when the meter has not been initialized we want await() to actually wait
-      // till the code initializes the meter
-      // if we don't add this catch block we'll throw MeterNotFoundException and await() code needs
-      // special handling to ensure the excpeiton is dealt with
-      // so instead of every called doing something like the example snippet below we just return 0
-      //      await()
-      //              .until(
-      //                      () -> {
-      //                        try {
-      //                          double count = getCount(METRIC_NAME, meterRegistry);
-      //                          LOG.debug("METRIC_NAME current_count={} total_count={}", count,
-      // meterRegistry);
-      //                          return count == <count_expected>;
-      //                        } catch (MeterNotFoundException e) {
-      //                          return false;
-      //                        }
-      //                      });
+    } catch (MeterNotFoundException e) {
+      LOG.error("Metric not found", e);
       return 0;
+    } catch (Exception e) {
+      LOG.error("Error while getting metric", e);
+      throw e;
     }
   }
 
   public static double getValue(String guageName, MeterRegistry metricsRegistry) {
     try {
       return metricsRegistry.get(guageName).gauge().value();
-    } catch (Exception e) {
+    } catch (MeterNotFoundException e) {
+      LOG.error("Metric not found", e);
       return 0;
+    } catch (Exception e) {
+      LOG.error("Error while getting metric", e);
+      throw e;
     }
   }
 
   public static double getTimerCount(String timerName, MeterRegistry metricsRegistry) {
     try {
       return metricsRegistry.get(timerName).timer().count();
-    } catch (Exception e) {
+    } catch (MeterNotFoundException e) {
+      LOG.error("Metric not found", e);
       return 0;
+    } catch (Exception e) {
+      LOG.error("Error while getting metric", e);
+      throw e;
     }
   }
 }
