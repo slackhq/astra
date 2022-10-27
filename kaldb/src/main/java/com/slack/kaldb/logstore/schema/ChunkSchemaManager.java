58a2a7d01c121e880961e6d45d3ed66eef537ed0
Suman Karumuri <skarumuri@salesforce.com>
2022-10-27 11:45:46 -0700

    Add metadata.proto for schema.

--- kaldb/src/main/java/com/slack/kaldb/logstore/schema/ChunkSchemaManager.java
+++ kaldb/src/main/java/com/slack/kaldb/logstore/schema/ChunkSchemaManager.java
@@ -1,12 +1,10 @@
 package com.slack.kaldb.logstore.schema;
 
-/**
- * ChunkSchemaManager class manages the schema for a chunk.
- */
+/** ChunkSchemaManager class manages the schema for a chunk. */
 public class ChunkSchemaManager {
 
-    // Serialize the field def map.
-    // De-serialize the field def map.
+  // Serialize the field def map.
+  // De-serialize the field def map.
 
-    // Initialize the default field schema. Static for now but read from a file in the future.
+  // Initialize the default field schema. Static for now but read from a file in the future.
 }
