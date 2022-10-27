c5a0eb4778b993d80755cfc96a20427d06f5725d
Suman Karumuri <skarumuri@salesforce.com>
2022-10-27 11:22:25 -0700

    Add a schema manager.

---
+++ kaldb/src/main/java/com/slack/kaldb/logstore/schema/ChunkSchemaManager.java
@@ -0,0 +1,12 @@
+package com.slack.kaldb.logstore.schema;
+
+/**
+ * ChunkSchemaManager class manages the schema for a chunk.
+ */
+public class ChunkSchemaManager {
+
+    // Serialize the field def map.
+    // De-serialize the field def map.
+
+    // Initialize the default field schema. Static for now but read from a file in the future.
+}
