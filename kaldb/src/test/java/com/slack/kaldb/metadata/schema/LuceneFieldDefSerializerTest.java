86ae639f34ccb7ae5b87980ad4131d8f02f4b0cf
Suman Karumuri <skarumuri@salesforce.com>
2022-10-28 14:57:05 -0700

    Add a serializer test.

---
+++ kaldb/src/test/java/com/slack/kaldb/metadata/schema/LuceneFieldDefSerializerTest.java
@@ -0,0 +1,48 @@
+package com.slack.kaldb.metadata.schema;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.slack.kaldb.logstore.schema.SchemaAwareLogDocumentBuilderImpl;
+import org.junit.Test;
+
+public class LuceneFieldDefSerializerTest {
+  private final LuceneFieldDefSerializer serDe = new LuceneFieldDefSerializer();
+
+  @Test
+  public void testLuceneFieldDefSerializer() throws InvalidProtocolBufferException {
+    String intFieldName = "IntfieldDef";
+    String intType = "integer";
+    LuceneFieldDef fieldDef = new LuceneFieldDef(intFieldName, intType, true, true, false, true);
+
+    String serializedFieldDef = serDe.toJsonStr(fieldDef);
+    assertThat(serializedFieldDef).isNotEmpty();
+
+    LuceneFieldDef deserializedFieldDef = serDe.fromJsonStr(serializedFieldDef);
+    assertThat(deserializedFieldDef).isEqualTo(fieldDef);
+
+    assertThat(deserializedFieldDef.name).isEqualTo(intFieldName);
+    assertThat(deserializedFieldDef.fieldType)
+        .isEqualTo(SchemaAwareLogDocumentBuilderImpl.FieldType.INTEGER);
+    assertThat(deserializedFieldDef.isStored).isTrue();
+    assertThat(deserializedFieldDef.isIndexed).isTrue();
+    assertThat(deserializedFieldDef.isAnalyzed).isFalse();
+    assertThat(deserializedFieldDef.storeNumericDocValue).isTrue();
+  }
+
+  @Test
+  public void testInvalidSerializations() {
+    Throwable serializeNull = catchThrowable(() -> serDe.toJsonStr(null));
+    assertThat(serializeNull).isInstanceOf(IllegalArgumentException.class);
+
+    Throwable deserializeNull = catchThrowable(() -> serDe.fromJsonStr(null));
+    assertThat(deserializeNull).isInstanceOf(InvalidProtocolBufferException.class);
+
+    Throwable deserializeEmpty = catchThrowable(() -> serDe.fromJsonStr(""));
+    assertThat(deserializeEmpty).isInstanceOf(InvalidProtocolBufferException.class);
+
+    Throwable deserializeCorrupt = catchThrowable(() -> serDe.fromJsonStr("test"));
+    assertThat(deserializeCorrupt).isInstanceOf(InvalidProtocolBufferException.class);
+  }
+}
