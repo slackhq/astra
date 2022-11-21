4d08c675e56b42d2e760f6397a65e0e56c328474
Suman Karumuri <skarumuri@salesforce.com>
2022-11-21 10:02:01 -0800

    tiny

--- kaldb/src/main/java/com/slack/kaldb/logstore/DocumentBuilder.java
+++ kaldb/src/main/java/com/slack/kaldb/logstore/DocumentBuilder.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.util.Map;
 import org.apache.lucene.document.Document;
 
+/**
+ * DocumentBuilder defines the interfaces for classes that generate Lucene documents out of
+ * messages.
+ */
 public interface DocumentBuilder<T> {
   Document fromMessage(T message) throws IOException;
 
