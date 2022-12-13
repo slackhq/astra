8afbb5c546d05291b24e204742a03aa78e8a3c8c
Dan Hermann <danhermann@users.noreply.github.com>
2022-12-13 11:12:53 -0600

    Adding tests for cases when Kafka offsets are NLA (#440)

--- kaldb/src/main/java/com/slack/kaldb/recovery/RecoveryService.java
+++ kaldb/src/main/java/com/slack/kaldb/recovery/RecoveryService.java
@@ -75,9 +75,11 @@ public class RecoveryService extends AbstractIdleService {
       "recovery_node_assignment_received";
   public static final String RECOVERY_NODE_ASSIGNMENT_SUCCESS = "recovery_node_assignment_success";
   public static final String RECOVERY_NODE_ASSIGNMENT_FAILED = "recovery_node_assignment_failed";
+  public static final String RECORDS_NO_LONGER_AVAILABLE = "records_no_longer_available";
   protected final Counter recoveryNodeAssignmentReceived;
   protected final Counter recoveryNodeAssignmentSuccess;
   protected final Counter recoveryNodeAssignmentFailed;
+  protected final Counter recoveryRecordsNoLongerAvailable;
   private SearchMetadataStore searchMetadataStore;
 
   public RecoveryService(
@@ -117,6 +119,8 @@ public class RecoveryService extends AbstractIdleService {
         meterRegistry.counter(RECOVERY_NODE_ASSIGNMENT_SUCCESS, meterTags);
     recoveryNodeAssignmentFailed =
         meterRegistry.counter(RECOVERY_NODE_ASSIGNMENT_FAILED, meterTags);
+    recoveryRecordsNoLongerAvailable =
+        meterRegistry.counter(RECORDS_NO_LONGER_AVAILABLE, meterTags);
   }
 
   @Override
@@ -245,6 +249,13 @@ public class RecoveryService extends AbstractIdleService {
               partitionOffsets.endOffset,
               recoveryTaskMetadata.createdTimeEpochMs);
 
+      if (partitionOffsets.startOffset != recoveryTaskMetadata.startOffset
+          || recoveryTaskMetadata.endOffset != partitionOffsets.endOffset) {
+        recoveryRecordsNoLongerAvailable.increment(
+            (partitionOffsets.startOffset - recoveryTaskMetadata.startOffset)
+                + (partitionOffsets.endOffset - recoveryTaskMetadata.endOffset));
+      }
+
       try {
         RecoveryChunkManager<LogMessage> chunkManager =
             RecoveryChunkManager.fromConfig(
@@ -283,6 +294,8 @@ public class RecoveryService extends AbstractIdleService {
         return false;
       }
     } else {
+      recoveryRecordsNoLongerAvailable.increment(
+          recoveryTaskMetadata.endOffset - recoveryTaskMetadata.startOffset + 1);
       LOG.info("Recovery task {} data no longer available in Kafka", recoveryTaskMetadata);
       return true;
     }
