3818a3609bd968895a4375376b570ca095fc7d6c
Bryan Burkholder <771133+bryanlb@users.noreply.github.com>
2023-09-21 17:13:34 -0700

    Change HPA to account for Kubernetes tolerances (#681)
    Co-authored-by: Bryan Burkholder <bryanlb@users.noreply.github.com>

--- kaldb/src/test/java/com/slack/kaldb/clusterManager/ClusterHpaMetricServiceTest.java
+++ kaldb/src/test/java/com/slack/kaldb/clusterManager/ClusterHpaMetricServiceTest.java
@@ -94,7 +94,6 @@ class ClusterHpaMetricServiceTest {
     ClusterHpaMetricService clusterHpaMetricService =
         new ClusterHpaMetricService(
             replicaMetadataStore, cacheSlotMetadataStore, hpaMetricMetadataStore);
-    clusterHpaMetricService.CACHE_SLOT_OVER_PROVISION = 1;
 
     when(replicaMetadataStore.listSync())
         .thenReturn(
@@ -167,10 +166,10 @@ class ClusterHpaMetricServiceTest {
             .findFirst()
             .get();
 
-    // 2 replicas, 3 slots (2+1)/(3+1)
-    assertThat(rep1Metadata.getValue()).isEqualTo(0.75);
+    // 2 replicas, 3 slots
+    assertThat(rep1Metadata.getValue()).isEqualTo(0.67);
 
-    // 1 replica, 1 slot (1+1)/(1+1)
+    // 1 replica, 1 slot
     assertThat(rep2Metadata.getValue()).isEqualTo(1.0);
   }
 
@@ -180,7 +179,6 @@ class ClusterHpaMetricServiceTest {
     ClusterHpaMetricService clusterHpaMetricService =
         new ClusterHpaMetricService(
             replicaMetadataStore, cacheSlotMetadataStore, hpaMetricMetadataStore);
-    clusterHpaMetricService.CACHE_SLOT_OVER_PROVISION = 1;
 
     when(replicaMetadataStore.listSync())
         .thenReturn(
@@ -251,13 +249,13 @@ class ClusterHpaMetricServiceTest {
             .findFirst()
             .get();
 
-    // only one should get a lock, the other should be (1 + 1) / (2 + 1))
-    if (rep1Metadata.getValue().equals(0.0)) {
-      assertThat(rep1Metadata.getValue()).isEqualTo(0.0);
-      assertThat(rep2Metadata.getValue()).isEqualTo(0.67);
+    // only one should get a lock, the other should be 1 replica, 2 slots
+    if (rep1Metadata.getValue().equals(1.0)) {
+      assertThat(rep1Metadata.getValue()).isEqualTo(1.0);
+      assertThat(rep2Metadata.getValue()).isEqualTo(0.5);
     } else {
-      assertThat(rep1Metadata.getValue()).isEqualTo(0.67);
-      assertThat(rep2Metadata.getValue()).isEqualTo(0.0);
+      assertThat(rep1Metadata.getValue()).isEqualTo(0.5);
+      assertThat(rep2Metadata.getValue()).isEqualTo(1.0);
     }
   }
 
@@ -267,7 +265,6 @@ class ClusterHpaMetricServiceTest {
     ClusterHpaMetricService clusterHpaMetricService =
         new ClusterHpaMetricService(
             replicaMetadataStore, cacheSlotMetadataStore, hpaMetricMetadataStore);
-    clusterHpaMetricService.CACHE_SLOT_OVER_PROVISION = 1;
 
     when(replicaMetadataStore.listSync())
         .thenReturn(
@@ -318,10 +315,10 @@ class ClusterHpaMetricServiceTest {
             .findFirst()
             .get();
 
-    // (2 + 1) / (1 + 1)
-    assertThat(rep1Metadata.getValue()).isEqualTo(1.5);
+    // 2 replicas, 1 slot
+    assertThat(rep1Metadata.getValue()).isEqualTo(2);
 
-    // (2 + 1) / 1
-    assertThat(rep2Metadata.getValue()).isEqualTo(3.0);
+    // 2 replicas, 0 slots (will log an error and return a default no-op)
+    assertThat(rep2Metadata.getValue()).isEqualTo(1);
   }
 }
