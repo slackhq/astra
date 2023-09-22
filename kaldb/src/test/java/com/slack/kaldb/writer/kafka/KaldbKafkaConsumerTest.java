8ca66cccee7c5cdd59a3722e58504430626f7c3e
Varun Thacker <varun@vthacker.in>
2023-09-22 14:45:55 -0700

    Upgrade to java 21 (#678)
    * Upgrade built to java 21
    
    * use corretto
    
    * upgrade Mockito
    
    * explicitly define bytebuddy definition
    
    * use new fmt-maven-plugin version
    
    * use com.spotify.fmt:fmt-maven-plugin
    
    * whitespace
    
    * comments
    
    * bytebuddy

--- kaldb/src/test/java/com/slack/kaldb/writer/kafka/KaldbKafkaConsumerTest.java
+++ kaldb/src/test/java/com/slack/kaldb/writer/kafka/KaldbKafkaConsumerTest.java
@@ -223,6 +223,7 @@ public class KaldbKafkaConsumerTest {
       // Assign doesn't create a consumer group.
       assertThat(kafkaServer.getConnectedConsumerGroups()).isEqualTo(0);
     }
+
     // TODO: Test batch ingestion with roll over. Not adding a test, since this functionality is
     // not needed by the recovery indexer yet.
 
