8ea307b3750d22687c21933a9a9d3bfc68ec80be
Varun Thacker <varun@vthacker.in>
2023-10-18 10:16:50 -0700

    Register OpenSearchBulkIngestApi as a service itself (#693)
    * logging
    
    * more changes
    
    * expose / endpoint
    
    * remove extra endpoints
    
    * fixup code comments

--- kaldb/src/main/java/com/slack/kaldb/server/Kaldb.java
+++ kaldb/src/main/java/com/slack/kaldb/server/Kaldb.java
@@ -394,8 +394,12 @@ public class Kaldb {
               KaldbConfigs.NodeRole.PREPROCESSOR, List.of(datasetMetadataStore)));
 
       if (preprocessorConfig.getUseBulkApi()) {
-        armeriaServiceBuilder.withAnnotatedService(
-            new OpenSearchBulkIngestApi(datasetMetadataStore, preprocessorConfig, meterRegistry));
+        // TODO: using an armeria service that is also a guava service does not look elegant
+        // explore ways where we can control the lifecycle without the need for a guava service here
+        OpenSearchBulkIngestApi openSearchBulkApiService =
+            new OpenSearchBulkIngestApi(datasetMetadataStore, preprocessorConfig, meterRegistry);
+        armeriaServiceBuilder.withAnnotatedService(openSearchBulkApiService);
+        services.add(openSearchBulkApiService);
       } else {
         PreprocessorService preprocessorService =
             new PreprocessorService(datasetMetadataStore, preprocessorConfig, meterRegistry);
