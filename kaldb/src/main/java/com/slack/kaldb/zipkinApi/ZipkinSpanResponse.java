392bdcc7eb5af843a7f372bdfc6a1426df7003f7
Bryan Burkholder <771133+bryanlb@users.noreply.github.com>
2023-10-25 17:20:16 -0700

    Refactor Zipkin API, fix duration bug (#710)
    Co-authored-by: Bryan Burkholder <bryanlb@users.noreply.github.com>

---
+++ kaldb/src/main/java/com/slack/kaldb/zipkinApi/ZipkinSpanResponse.java
@@ -0,0 +1,110 @@
+package com.slack.kaldb.zipkinApi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+
+/**
+ * Span response object for Zipkin API
+ *
+ * @see <a href="https://zipkin.io/zipkin-api/#/">Zipkin API Spec</a>
+ */
+@SuppressWarnings("unused")
+public class ZipkinSpanResponse {
+  private final String id;
+  private final String traceId;
+  private String parentId = null;
+  private String name = null;
+
+  @JsonProperty("timestamp")
+  private Long timestampMicros = null;
+
+  private ZipkinEndpointResponse localEndpoint = null;
+
+  private ZipkinEndpointResponse remoteEndpoint = null;
+
+  @JsonProperty("duration")
+  // Zipkin spec defines this is integer, even though a long seems like it would be more appropriate
+  private int durationMicros;
+
+  private String kind;
+
+  private Map<String, String> tags;
+
+  public ZipkinSpanResponse(String id, String traceId) {
+    // id and traceId are only required fields
+    this.id = id;
+    this.traceId = traceId;
+  }
+
+  public void setParentId(String parentId) {
+    this.parentId = parentId;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setTimestamp(long timestampMicros) {
+    this.timestampMicros = timestampMicros;
+  }
+
+  public void setDuration(int durationMicros) {
+    this.durationMicros = durationMicros;
+  }
+
+  public void setKind(String kind) {
+    this.kind = kind;
+  }
+
+  public void setTags(Map<String, String> tags) {
+    this.tags = tags;
+  }
+
+  public ZipkinEndpointResponse getLocalEndpoint() {
+    return localEndpoint;
+  }
+
+  public void setLocalEndpoint(ZipkinEndpointResponse localEndpoint) {
+    this.localEndpoint = localEndpoint;
+  }
+
+  public ZipkinEndpointResponse getRemoteEndpoint() {
+    return remoteEndpoint;
+  }
+
+  public void setRemoteEndpoint(ZipkinEndpointResponse remoteEndpoint) {
+    this.remoteEndpoint = remoteEndpoint;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getTraceId() {
+    return traceId;
+  }
+
+  public String getParentId() {
+    return parentId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public long getTimestamp() {
+    return timestampMicros;
+  }
+
+  public int getDuration() {
+    return durationMicros;
+  }
+
+  public String getKind() {
+    return kind;
+  }
+
+  public Map<String, String> getTags() {
+    return tags;
+  }
+}
