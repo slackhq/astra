fc3adcc1c719e950bc74038b337cc053c5e1842d
vthacker <varun@vthacker.in>
2024-05-08 12:52:41 -0700

    change error logging for opensearchadapter

--- astra/src/main/java/com/slack/astra/logstore/opensearch/OpenSearchAdapter.java
+++ astra/src/main/java/com/slack/astra/logstore/opensearch/OpenSearchAdapter.java
@@ -507,7 +507,7 @@ public class OpenSearchAdapter {
             new CompressedXContent(BytesReference.bytes(mapping)),
             MapperService.MergeReason.MAPPING_UPDATE);
       } catch (Exception e) {
-        LOG.error("Error doing map update", e);
+        LOG.error("Error doing map update errorMsg={}", e.getMessage());
       }
       return true;
     }
