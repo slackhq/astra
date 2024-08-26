850b4a664cfc9b5ab4a3a0ec7a3ccaf277f9e9c2
Emma Montross <emontross@slack-corp.com>
2024-08-26 10:51:25 -0600

    error log on chunk failure

--- astra/src/main/java/com/slack/astra/logstore/search/AstraDistributedQueryService.java
+++ astra/src/main/java/com/slack/astra/logstore/search/AstraDistributedQueryService.java
@@ -418,6 +418,7 @@ public class AstraDistributedQueryService extends AstraQueryServiceBase implemen
               response.add(searchResult.get() == null ? SearchResult.error() : searchResult.get());
             } else {
               response.add(SearchResult.error());
+              LOG.error("Error fetching search result {}", searchResult);
             }
           } catch (Exception e) {
             LOG.error("Error fetching search result", e);
