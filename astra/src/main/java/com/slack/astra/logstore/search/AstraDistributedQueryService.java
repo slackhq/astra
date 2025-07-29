f34192af475899952cbc7018bd1263222f8726c5
Kyle Sammons <ksammons@slack-corp.com>
2025-07-29 09:55:55 -0700

    Fix

--- astra/src/main/java/com/slack/astra/logstore/search/AstraDistributedQueryService.java
+++ astra/src/main/java/com/slack/astra/logstore/search/AstraDistributedQueryService.java
@@ -760,7 +760,7 @@ public class AstraDistributedQueryService extends AstraQueryServiceBase implemen
     } catch (Exception e) {
       LOG.error("Search failed with ", e);
       span.error(e);
-      failedQueryCount.increment();
+//      failedQueryCount.increment();
       return searchMetadataURLToSnapshotNames.entrySet().stream()
           .collect(Collectors.toMap(Map.Entry::getKey, _ -> SearchResult.error()));
     }
@@ -905,7 +905,7 @@ public class AstraDistributedQueryService extends AstraQueryServiceBase implemen
     } catch (Exception e) {
       LOG.error("Search failed with ", e);
       span.error(e);
-      failedQueryCount.increment();
+//      failedQueryCount.increment();
       return List.of(SearchResult.empty());
     } finally {
       recordQueryDuration(requestedDataHours, Instant.now().toEpochMilli() - startTime, "old");
