16dd86697a105ca32b4ad8a2452a4e0962968876
Emma Montross <emontross@slack-corp.com>
2025-09-17 16:30:28 -0600

    add revision log

--- astra/src/main/java/com/slack/astra/metadata/core/EtcdMetadataStore.java
+++ astra/src/main/java/com/slack/astra/metadata/core/EtcdMetadataStore.java
@@ -864,6 +864,11 @@ public class EtcdMetadataStore<T extends AstraMetadata> implements Closeable {
       // This ensures we don't miss events that occur during watch registration
       watchOption =
           WatchOption.builder().withPrefix(prefix).withRevision(currentRevision + 1).build();
+      LOG.info(
+          "adding listener {} for store {} at revision {}",
+          listener,
+          storeFolder,
+          currentRevision + 1);
     } catch (InterruptedException | ExecutionException | TimeoutException e) {
       LOG.error("Failed to get current revision for watch setup on attempt {}", attemptNumber, e);
       // Fallback to basic watch without revision
